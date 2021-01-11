52d4cf45f61f2badcc2112da83d469d6f0bf7f5f
igorgo-hyuna <igor.gologots@hyuna.bb>
2021-01-11 18:21:44 +0200

    Added 3 sections

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    
     /* Burger */
     $(".toggle_mnu").click(function() {
         $(".sandwich").toggleClass("active");
@@ -19,6 +20,30 @@ $(document).ready(function() {
             $(".top_text").css("opacity", ".1");
             $(".top_mnu").fadeIn(600);
         };
+    });
+
+    /* Owl carousel */
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            960: {
+                items: 4
+            },
+            1200: {
+                items: 4
+            }
+        }
     });
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -20,14 +20,14 @@ gulp.task('scss', function(){
 		.pipe(browserSync.reload({stream: true}))
 });
 
-/**************************** Таск, для подготовки к сжатию css ***************************/
+// *************************** Таск, для подготовки к сжатию css **************************
 gulp.task('scripts', function(){
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/bootstrap/bootstrap.min.js',
+		'app/libs/owlCarousel2-2.3.4/dist/owl.carousel.min.js',
 		// 'app/libs/parallax.js-1.5.0/parallax.min.js',
 		// 'app/libs/pageScrool2/PageScroll2id.min.js',
-		// 'app/libs/owlCarousel2-2.3.4/dist/owl.carousel.min.js',
 		// 'app/libs/jqueryCounter/counter.lib.js',
 		// 'app/libs/jqueryCounter/jquery.counterup.min.js',
 		// 'app/libs/backgroundvideo/jquery.backgroundvideo.min.js',
